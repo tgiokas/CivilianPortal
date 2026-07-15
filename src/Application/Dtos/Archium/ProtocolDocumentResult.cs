5c2e5d61d6fbd4ef5c280722fb7ad1e72a22f6f8
PGiokas <giokasp@cbs.gr>
2026-07-15 20:01:54 +0300

    Update ProtocolDocumentResult.cs

--- src/Application/Dtos/Archium/ProtocolDocumentResult.cs
+++ src/Application/Dtos/Archium/ProtocolDocumentResult.cs
@@ -1,9 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace CitizenPortal.Application.Dtos;
 
 public class ProtocolDocumentResult
 {
+    [JsonPropertyName("fileId")]
     public long FileId { get; set; }
+    [JsonPropertyName("protocolNumber")]
     public string? ProtocolNumber { get; set; }
+    [JsonPropertyName("protocolYear")]
     public string? ProtocolYear { get; set; }
+    [JsonPropertyName("timestamp")]
     public DateTime Timestamp { get; set; }
 }
